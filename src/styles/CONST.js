a234a0b3d0372916c7deb5c1cfc26050498782c9
Alexey Zapolskiy <alexey.z@cronix.ms>
2018-06-14 11:50:56 +0300

    Add constants styles

---
+++ src/styles/CONST.js
@@ -0,0 +1,42 @@
+import {height, width, totalSize} from 'react-native-dimension'
+
+export default {
+    colors: {
+        white: '#FFFFFF',
+        background: '#FCFCFC',
+        black: '#000000',
+        green: '#00DD00',
+
+        gradientRedDark: '#9F041B',
+        gradientRedLight: '#F5515F',
+
+        gradientBlueLight: '#1F97C9',
+        gradientBlueDark: '#4F45D0',
+
+        grey: '#808080',
+        search: '#d5d5d5',
+
+        gradientGreyLight: '#e2ebf0',
+        gradientGreyDark: '#cfd9df',
+
+        gold: '#DAA520',
+
+        gradientSummerDogGreen: '#a8ff78',
+        gradientSummerDogBlue: '#78ffd6',
+
+        gradientSubstrateLight: '#FFEFBA',
+        gradientSubstrateDark: '#FFFFFF',
+
+        gradientGoldLight: '#CAC531',
+        gradientGoldDark: '#F3F9A7'
+    },
+
+    container: {
+        flex: 1,
+    },
+
+    flexCenter: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+}
